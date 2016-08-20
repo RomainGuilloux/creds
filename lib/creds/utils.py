c7cba81aa60a4aabcf9210dca962f36faf7f9639
Jon Hadfield <jon@lessknown.co.uk>
2016-08-20 14:35:24 +0100

    WIP.

--- lib/creds/utils.py
+++ lib/creds/utils.py
@@ -33,6 +33,7 @@ def check_platform():
 
 
 def execute_command(command=None):
+    print(command)
     """Execute a command and return the stdout and stderr."""
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     return process.communicate()
